be36564e2da6912125a6879768d36b8e5da3da51
@Titus0406 <titusmbui01@gmail.com>
2024-09-24 05:01:18 -0700

    first commit

---
+++ Contact Us/contact.js
@@ -0,0 +1,34 @@
+   //toggle button for navbar
+   function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+  }
+
+  // Detect scroll and change the navbar background
+  window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('nav');
+    if (window.scrollY > 50) {  // 50 is pexels ya kubadilika
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+    // Back to Top Button functionality
+    var mybutton = document.getElementById("backToTopBtn");
+
+    // Show the button when the user scrolls down 100px
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            mybutton.style.display = "block";
+        } else {
+            mybutton.style.display = "none";
+        }
+    }
+
+    // Scroll to the top when the user clicks the button
+    function topFunction() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
